eb4f403b474fb8c485e403c4763761d210a5ff0b
Harrisson Dutra <harrissondutra@gmail.com>
2025-10-16 19:00:57 -0300

    Simplificado _app.tsx: Removido providers que causavam problemas durante o build estático Removido ThemeProvider: Que estava causando conflitos com useContext Removido AuthProvider: Que estava causando problemas de SSR

--- src/frontend/src/pages/_app.tsx
+++ src/frontend/src/pages/_app.tsx
@@ -1,19 +1,6 @@
 import type { AppProps } from 'next/app'
-import { AuthProvider } from '../components/providers/auth-provider'
-import { ThemeProvider } from '../components/providers/theme-provider'
 import '../app/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
-    </ThemeProvider>
-  )
+  return <Component {...pageProps} />
 }
